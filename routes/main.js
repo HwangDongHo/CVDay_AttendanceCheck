0f0e60f4404844bc906bcfcc38380cf67020325e
HwangDongHo <ygekl8523@naver.com>
2019-03-28 02:10:54 +0900

    add qrcode test

--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var db = mysql.createConnection({
+  host:'db.cpwxmow3ltbq.ap-northeast-2.rds.amazonaws.com',
+  user:'cvserver',
+  password:'ehdghghkd1',
+  database:'TF',
+  port: 3306
+});
+
+db.connect(function(error){
+  if(!!error)
+  {
+    console.log(error);
+    console.log('Error');
+  }
+  else
+  {
+    console.log('DB connected!');
+  }
+});
+
+
 
 router.post('/main', function(req, res, next) {
   res.render('qrcodetest.html');

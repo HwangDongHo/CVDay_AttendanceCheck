70832d70372879bd4079a32343681c421dfdef97
HwangDongHo <ygekl8523@naver.com>
2019-04-29 09:22:22 +0900

    add admin mode

--- routes/admin.js
+++ routes/admin.js
@@ -12,10 +12,35 @@ router.get('/admin/main', function(req, res, next) {
     var query = `select * from account`;
     var param = '';
 
+    var query1 = `select a.id,a.name,a.stu_num,a.email,a.kakaoid,a.phone_num,a.created,COUNT(*) AS cnt,sum(how_late) as total_late from account as a join late_log as b on a.stu_num = b.stu_num where month(b.check_time)= month(now()) AND how_late != 0 GROUP BY stu_num order by total_late DESC;`;
+
     sql.query(function (err, check) {
         if (err) console.log(err);
         if (check[0]) {
-            res.render('index_admin2.html',{account:check});
+
+            sql.query(function (err, log) {
+                if (err) console.log(err);
+                if (log[0]) {
+                    var rank = new Array();
+                    var cnt = new Array();
+                    var total_late = new Array();
+                    for(var i =0;i<check.length;i++){
+                        for(var j =0;j<log.length;j++){
+                            if(check[i].stu_num == log[j].stu_num){
+                                rank[i] = j+1;
+                                cnt[i] = log[j].cnt;
+                                total_late = log[j].total_late;
+                            }
+                        }
+                    }
+
+                    res.render('index_admin2.html',{account:check,rank:rank,cnt:cnt,total_late:total_late});
+                }
+            }, query1, param);
+
+
+
+
         }
     }, query, param);
 
